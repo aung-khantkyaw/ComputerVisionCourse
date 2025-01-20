85a1db349706be6c3e1809c63a6f69ee62f65945
aung-khantkyaw <aungkhantkyaw.19-20@ucsmub.edu.mm>
2025-01-20 10:26:36 +0630

    Lecture 4

--- CS_4113-PUMUB/ex_ten.py
+++ CS_4113-PUMUB/ex_ten.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 img = cv2.imread('lena.png', 0)
 img_frequency = np.fft.fft2(img)
@@ -13,14 +14,33 @@ D0 = 10
 for u in range(M):
     for v in range(N):
         D = np.sqrt((u-M/2)**2 + (v-N/2)**2)
-        H = np.exp(-D**2/(2*D0**2))
+        H[u, v] = np.exp(-D**2/(2*D0**2))
+
+high_pass = 1 - H
 
 #Image Filter
+#Low Pass Filter
 Gaussian = img_shift*H
 invert_shifted = np.fft.ifftshift(Gaussian)
-Filter = np.fft.ifft2(invert_shifted)
+low_pass_filter = np.fft.ifft2(invert_shifted)
 
-cv2.imshow('Original', img)
-cv2.imshow('Filtered', np.abs(Filter))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#Low Pass Filter
+Gaussian = img_shift*high_pass
+invert_shifted = np.fft.ifftshift(Gaussian)
+high_pass_filter = np.fft.ifft2(invert_shifted)
+
+plt.subplot(131)
+plt.imshow(img)
+plt.title("Original Image")
+
+plt.subplot(132)
+plt.imshow(np.abs(low_pass_filter))
+plt.title("Low Pass Filtered")
+
+plt.subplot(133)
+plt.imshow(np.abs(high_pass_filter))
+plt.title("High Pass Filtered")
+
+plt.show()
+
+# Frequency => image ကို transform ပြောင်း => fft2(fftshift)
